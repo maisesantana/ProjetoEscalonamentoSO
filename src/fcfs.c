3a91970026dd25a040495237ea0c8acd8ddf2e91
Maise <maisesantana17@gmail.com>
2025-11-21 20:55:43 -0300

    Update fcfs.c

--- src/fcfs.c
+++ src/fcfs.c
@@ -2,45 +2,32 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, j;
+    int n, i;
+
     printf("Quantos processos? ");
     scanf("%d", &n);
 
-    int bt[n], wt[n], tat[n], p[n], temp;
+    int bt[n], wt[n], tat[n];
 
     printf("Digite o Burst Time de cada processo:\n");
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         printf("P%d: ", i + 1);
         scanf("%d", &bt[i]);
-        p[i] = i + 1;
-    }
-
-    for(i = 0; i < n - 1; i++) {
-        for(j = i + 1; j < n; j++) {
-            if(bt[j] < bt[i]) {
-                temp = bt[i];
-                bt[i] = bt[j];
-                bt[j] = temp;
-
-                temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
-            }
-        }
     }
 
     wt[0] = 0;
-    for(i = 1; i < n; i++) {
+
+    for (i = 1; i < n; i++) {
         wt[i] = wt[i - 1] + bt[i - 1];
     }
 
-    for(i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         tat[i] = wt[i] + bt[i];
     }
 
     printf("\nProcesso\tBT\tWT\tTAT\n");
-    for(i = 0; i < n; i++) {
-        printf("P%d\t\t%d\t%d\t%d\n", p[i], bt[i], wt[i], tat[i]);
+    for (i = 0; i < n; i++) {
+        printf("P%d\t\t%d\t%d\t%d\n", i + 1, bt[i], wt[i], tat[i]);
     }
 
     return 0;
